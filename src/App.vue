669168ef3cd0615023d06b46576e76c407ebf409
Alim4ik322 <assanovali23@gmail.com>
2025-05-13 02:52:36 +0300

    4

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import UserInfo from './components/UserInfo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    UserInfo
+  }
+}
+</script>
+
+<template>
+  <div>
+    <UserInfo />
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/UserInfo.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div>{{ name }}</div>
+    <div>{{ surn }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'john',
+      surn: 'smit'
+    }
+  }
+}
+</script>
+
+<style scoped>
+div {
+  margin: 10px 0;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+</style> 
